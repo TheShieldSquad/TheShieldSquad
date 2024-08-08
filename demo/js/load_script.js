d3534a772f00f6698a48f1ec2d6f6e079b399dbc
Justin P Mathew <73437505+iamjusttin@users.noreply.github.com>
2024-08-08 19:13:35 +0530

    Update load_script.js

--- demo/js/load_script.js
+++ demo/js/load_script.js
@@ -2,6 +2,6 @@ window.onload = function() {
     setTimeout(function() {
         document.getElementById('buffering').style.display = 'none';
         document.getElementById('index').style.display = 'block';
-        document.getElementById('themeStylesheet').setAttribute('href', 'css/main.css');
+        document.getElementById('themeStylesheet').setAttribute('href', 'css/style.css');
     }, 3000);
 };
