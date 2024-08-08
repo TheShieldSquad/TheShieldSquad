6b08a817fb2a8e745840ec194b0cd8012356bb66
Justin P Mathew <73437505+iamjusttin@users.noreply.github.com>
2024-08-08 18:47:57 +0530

    Update load_script.js

--- demo/js/load_script.js
+++ demo/js/load_script.js
@@ -1,17 +1,7 @@
-function loadMainContent() {
-    // Create an iframe to load the index.html
-    const iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-    iframe.src = 'index.html';
-    document.body.appendChild(iframe);
-
-    iframe.onload = function() {
-        // Redirect to index.html after the iframe has loaded
-        window.location.href = 'main.html';
-    };
-}
-
-// Start loading index.html in the iframe after the loader is displayed
 window.onload = function() {
-    setTimeout(loadMainContent, 3000); // Show loader for 3 seconds
+    setTimeout(function() {
+        document.getElementById('buffering').style.display = 'none';
+        document.getElementById('index').style.display = 'block';
+        document.getElementById('themeStylesheet').setAttribute('href', 'css/main.css');
+    }, 3000);
 };
