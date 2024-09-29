8c5a3d5000382f9c1c3d90d04db6b5677ac21696
TRASHZ403 <z403trash@proton.me>
2024-09-29 11:48:30 -0500

    Delete demo directory

--- demo/js/load_script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    setTimeout(function() {
-        document.getElementById('buffering').style.display = 'none';
-        document.getElementById('index').style.display = 'block';
-        document.getElementById('themeStylesheet').setAttribute('href', 'css/style.css');
-    }, 3000);
-};
